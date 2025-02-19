d852f9d9d8820fbb409caa66dcca61f1214ab1aa
sundarsharmaspk2023 <sundarsharmaspk2023@gmail.com>
2025-02-19 23:21:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import http from "http";
 
 const server = http.createServer((req, res)=>{
        res.writeHead(200,{'Content-Type':'text/plain'});
-       res.end('Hello word1');
+       res.end('Hello word');
 })
 
 server.listen(8000,'0.0.0.0', ()=>{
