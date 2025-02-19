15e67a67bc47191cfd7f4e02c6745a969223d471
sundarsharmaspk2023 <sundarsharmaspk2023@gmail.com>
2025-02-19 23:59:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import http from "http";
 
 const server = http.createServer((req, res)=>{
        res.writeHead(200,{'Content-Type':'text/plain'});
-       res.end('Sunder sharma ji. you are best');
+       res.end('Sunder sharma ji. you are best. you are mern developer');
 })
 
 server.listen(8000,'0.0.0.0', ()=>{
