3607c332f528f7467369c6267e718b8d63849e1d
sundarsharmaspk2023 <sundarsharmaspk2023@gmail.com>
2025-02-19 14:27:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res)=>{
 })
 
 server.listen(3000, ()=>{
-    console.log('Server running at http://localhost:3000');
+    console.log('Server running at this http://localhost:3000');
 })
